1b96922444426477d7a3604dcddbc19e293949e8
user <vitorenesduarte@gmail.com>
2016-01-11 17:23:23 +0000

    Fix process_graph_config script: only create cmds for replicas in the topology config -> being defined in replicas.cfg is not enough

--- topologies/process_graph_config.sh
+++ topologies/process_graph_config.sh
@@ -2,6 +2,7 @@
 
 import sys
 from os import system
+from sets import Set
 
 if (len(sys.argv) < 4):
   print "Usage: " + sys.argv[0] + " REPLICAS_CONFIG TOPOLOGY_CONFIG REPLICAS_COMMANDS_DIR"
@@ -12,26 +13,43 @@ topology_config = sys.argv[2]
 replicas_commands_dir = sys.argv[3]
 system("rm -f " + replicas_commands_dir + "*.cmds")
 
-replicas = {}
+config = {}
 commands = {}
+replicas = Set()
 
 with open(replicas_config, "r") as file:
   for line in file:
     id,host,port = (i.rstrip("\n") for i in line.split(" "))
-    replicas[id] = [host, port]
-    commands[id] = []
+    config[id] = [host, port]
 
 with open(topology_config, "r") as file:
   for line in file:
     fst,snd = (i.rstrip("\n") for i in line.split(" "))
-    sndConfig = replicas[snd]
-    sndHost = sndConfig[0]
-    sndPort = sndConfig[1]
-    commands[fst].append("connect " + snd + ":" + sndHost + ":" + sndPort)
 
+    replicas.add(fst)
+    replicas.add(snd)
 
-for replicaId, connects in commands.iteritems():
-  with open(replicas_commands_dir + replicaId + ".cmds", "w") as file:
+    snd_config = config[snd]
+    snd_host = snd_config[0]
+    snd_port = snd_config[1]
+    connect = "connect " + snd + ":" + snd_host + ":" + snd_port
+
+    if(fst in commands):
+      commands[fst].append(connect)
+    else:
+      commands[fst] = [connect]
+
+for replica_id, connects in commands.iteritems():
+  replicas.remove(replica_id)
+  with open(replicas_commands_dir + replica_id + ".cmds", "w") as file:
     for connect in connects:
       file.write(connect + "\n")
+
+# if replicas is empty, then I have at least one command for each replica
+# if not, then there are replicas that do not have any command
+# but I should create the command file even so
+for replica_id in replicas:
+  with open(replicas_commands_dir + replica_id + ".cmds", "w") as file:
+    file.write("\n")
+
 
