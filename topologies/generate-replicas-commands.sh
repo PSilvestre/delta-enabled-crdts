e320c3ee769e7a5dab80d739875910037c8bee2c
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-03-14 14:09:10 +0000

    Update scripts in order to generate updates at different rates than we synchronize with replicas (1/5, 1/1 and 5/1)

--- topologies/generate-replicas-commands.sh
+++ topologies/generate-replicas-commands.sh
@@ -5,22 +5,21 @@ from os import system
 from sets import Set
 from random import randint
 
-wait_time_between_updates = 120
-max_update_number = 1000000
+wait_time_between_updates = 5
+rounds_of_updates = 42
+max_update_number = 100 * 1000 * 1000
 
 def random_update():
   return "add " + str(randint(0, max_update_number)) + "\n"
 
-if (len(sys.argv) < 4):
-  print "Usage: " + sys.argv[0] + " REPLICAS_CONFIG TOPOLOGY_CONFIG REPLICAS_COMMANDS_DIR [ROUNDS_OF_UPDATES]"
+if (len(sys.argv) < 5):
+  print "Usage: " + sys.argv[0] + " REPLICAS_CONFIG TOPOLOGY_CONFIG REPLICAS_COMMANDS_DIR UPDATES_PER_ROUND"
   sys.exit()
 
-rounds_of_updates = 1
 replicas_config = sys.argv[1]
 topology_config = sys.argv[2]
 replicas_commands_dir = sys.argv[3]
-if(len(sys.argv) > 4):
-  rounds_of_updates = int(sys.argv[4])
+updates_per_round = int(sys.argv[4])
 
 system("rm -f " + replicas_commands_dir + "*.cmds")
 
@@ -53,19 +52,14 @@ with open(topology_config, "r") as file:
 for replica_id, connects in commands.iteritems():
   replicas.remove(replica_id)
   with open(replicas_commands_dir + replica_id + ".cmds", "w") as file:
-    ROU = rounds_of_updates
     file.write("wait 5\n")
     for connect in connects:
       file.write(connect + "\n")
 
-    # this allows us to ignore acks resultant from connects
-    file.write("wait 5\n")
-    file.write(random_update())
-
-    while (ROU > 1):
+    for i in range(0, rounds_of_updates):
       file.write("wait " + str(wait_time_between_updates) + "\n")
-      file.write(random_update())
-      ROU -= 1
+      for j in range(0, updates_per_round):
+        file.write(random_update())
 
 # if replicas is empty, then I have at least one command for each replica
 # if not, then there are replicas that do not have any command
--- topologies/run-all-configurations.sh
+++ topologies/run-all-configurations.sh
@@ -7,7 +7,7 @@ system("rm -f analyse-all.sh")
 
 executions_per_config = 30
 gossip_sleep_time = 5
-rounds_of_updates = 10
+updates_per_round_list = [1, 5, 25]
 
 log_dir = "log/"
 cmd_dir = "cmd/"
@@ -36,40 +36,41 @@ h_config['fanouts'] = [-1, 1, 2]
 configs = [e_config, r_config, h_config]
 current_execution = 0
 
-for execution in range(executions_per_config):
-  print "echo \"Execution " + str(execution) + "\""
-  print "rm -f .PIDS"
-
-  for config in configs:
-    for fanout in config['fanouts']:
-      for delta_or_full_state in ["d", "s"]:
-        dirname = config['tag'] + "_f"
-        if fanout != -1:
-          dirname += str(fanout)
-
-        dirname += "_" + delta_or_full_state + "_" + str(execution)
-        replicas_config = replicas_config_dir + str(current_execution) + ".cfg"
-        cmd = cmd_dir + dirname + "/"
-        log = log_dir + dirname + "/"
-        delta_flag = "-" + delta_or_full_state
-
-        generate_command = "./generate-replicas-commands.sh {replicas_config} {topology_config} {cmd} {updates}".format(replicas_config = replicas_config, topology_config = config['file'], cmd = cmd, updates = rounds_of_updates)
-        start_command = "./start-replicas.sh {replicas_config} {cmd} {log} {gossip} {fanout} {delta_flag} | bash".format(replicas_config = replicas_config, cmd = cmd, log = log, gossip = gossip_sleep_time, fanout = fanout, delta_flag = delta_flag)
-
-        # create diretories if not present
-        print "mkdir -p " + cmd + " " + log
-        # clean those diretories 
-        print "rm -f " + cmd + "* " + log + "*"
-
-        print generate_command
-        print start_command
-
-        current_execution += 1
-
-        print "echo \"./analyse.sh " + log + " " + analysis_dir + "\" >> analyse-all.sh"
+for updates_per_round in updates_per_round_list:
+  for execution in range(executions_per_config):
+    print "echo \"Execution " + str(execution) + "\""
+    print "rm -f .PIDS"
   
-  print "sleep 20m"
-  print "./stop-replicas.sh"
+    for config in configs:
+      for fanout in config['fanouts']:
+        for delta_or_full_state in ["d", "s"]:
+          dirname = config['tag'] + "_f"
+          if fanout != -1:
+            dirname += str(fanout)
+  
+          dirname += "_" + delta_or_full_state + "_" + str(execution)
+          replicas_config = replicas_config_dir + str(current_execution) + ".cfg"
+          cmd = cmd_dir + str(updates_per_round) + "/" + dirname + "/"
+          log = log_dir + str(updates_per_round) + "/" + dirname + "/"
+          delta_flag = "-" + delta_or_full_state
+  
+          generate_command = "./generate-replicas-commands.sh {replicas_config} {topology_config} {cmd} {updates}".format(replicas_config = replicas_config, topology_config = config['file'], cmd = cmd, updates = updates_per_round)
+          start_command = "./start-replicas.sh {replicas_config} {cmd} {log} {gossip} {fanout} {delta_flag} | bash".format(replicas_config = replicas_config, cmd = cmd, log = log, gossip = gossip_sleep_time, fanout = fanout, delta_flag = delta_flag)
+  
+          # create diretories if not present
+          print "mkdir -p " + cmd + " " + log
+          # clean those diretories 
+          print "rm -f " + cmd + "* " + log + "*"
+  
+          print generate_command
+          print start_command
+  
+          current_execution += 1
+  
+          print "echo \"./analyse.sh " + log + " " + analysis_dir + str(updates_per_round) + "/\" >> analyse-all.sh"
+    
+    print "sleep 5m"
+    print "./stop-replicas.sh"
 
 print "chmod u+x analyse-all.sh"
 print "echo \"I'm done!\""
